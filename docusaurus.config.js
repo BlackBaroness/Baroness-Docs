16186678b2facc082284f7324b9ccfa398929111
BlackBaroness <shoyurash2@gmail.com>
2021-09-12 16:17:38 +0300

    Script is ready

---
+++ docusaurus.config.js
@@ -0,0 +1,70 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'Baroness\'Docs',
+  tagline: '',
+  url: 'https://baronessdev.ru',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/logo.png',
+  organizationName: 'BaronessDev', // Usually your GitHub org/user name.
+  projectName: 'BaronessDev', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          // Please change this to your repo.
+          editUrl: undefined,
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: undefined
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Baroness\'Docs',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.png',
+        },
+        items: [],
+      },
+      footer: {
+        style: 'dark',
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} BaronessDev. <br> Сделано с ❤ <a href="https://vk.com/xiliken" target="_blank">Евгений Базоров</a>`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      colorMode: {
+        defaultMode: "dark",
+      },
+    }),
+    "i18n": {
+      "defaultLocale": "ru",
+      "locales": [
+        "ru"
+      ],
+    }
+});
